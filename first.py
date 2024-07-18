3de890ee4e2108bd5a95b75fcc3ee83d063fe715
sanchita <sanchita15pa@gmail.com>
2024-07-18 19:38:25 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,36 @@
+#print("....hiii....")
+#x=10
+#y=10
+#print(id(x))
+#print(id(y))
+x=10
+y=10
+print(type(x))
+print(type(y))
+str='''i
+        love
+             you'''
+print(type(str))
+lis=[10,20,3,'sanchita']
+print(type(lis))
+tup=(10,20,30,40,"sanchita")
+print(type(tup))
+dic={"10:h","20:u","30:o","hii:j"}
+print(type(dic))
+str='python'
+print(str.index('h'))
+my_list=[10,20,30,40,50,60,'sanchu']
+print(my_list.index(40))
+#print(my_list.index('neeraj'))
+print(my_list.index('sanchu'))
+str='laungage'
+print(str.index("a"))
+x=3
+print(type(x))
+
+x=3.5
+print(type(x))
+
+x=3+3j
+print(type(x))
+
---
+++ note.py
@@ -0,0 +1,16 @@
+'''data type
+numeric
+string
+sequential-collection of indexing called sequential data type
+three type
+list--collection of object
+tuple--collection of object small braket 
+range
+map data type
+dict--key and  value format key always unike 
+set data type
+set
+frozonset
+duplicate value not allow  usnig set
+binary dta type
+byte array in range(0 to255)'''
